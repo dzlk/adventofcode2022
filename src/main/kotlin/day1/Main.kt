d311d9f94190b40bd1b781583598961bd145cf34
Evgeny <dlb.diesel@gmail.com>
2022-12-05 09:48:17 +0500

    day 1

---
+++ src/main/kotlin/day1/Main.kt
@@ -0,0 +1,66 @@
+package day1
+
+import java.io.File
+
+class Main
+
+fun main(args: Array<String>) {
+    val resource = Main::class.java.classLoader.getResource("day1/input") ?: return
+    val file = File(resource.toURI())
+
+    val counter = Counter()
+
+    file.forEachLine {
+        var value : Int? = null
+        try {
+            value = it.toInt()
+        } catch (e : NumberFormatException) {
+            // do nothing
+        }
+
+        counter.count(value)
+    }
+
+    println("max: " + counter.getMax())
+    println("sum: " + counter.getTopSum())
+}
+
+class Counter {
+    private var curr : Int = 0
+    private var top : MutableList<Int> = listOf(0, 0, 0).toMutableList()
+
+    private fun updateTop() {
+        if (curr <= top[top.count() - 1]) {
+            curr = 0
+            return
+        }
+
+        top.forEachIndexed { index, value ->
+            if (curr > value) {
+                top[index] = curr
+                curr = value
+            }
+        }
+
+        curr = 0
+    }
+
+    fun count(value: Int?) {
+        if (value == null) {
+            updateTop()
+            return
+        }
+
+        curr += value
+    }
+
+    fun getMax() : Int {
+        updateTop()
+
+        return top[0]
+    }
+
+    fun getTopSum() : Int {
+        return top.reduce { acc, value -> acc + value }
+    }
+}
