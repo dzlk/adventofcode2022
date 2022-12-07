8d510440e6655b0af774e0dea9d703890ac8d136
Evgeny <dlb.diesel@gmail.com>
2022-12-07 09:27:41 +0500

    Refactoring day4

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,48 +1,49 @@
 package day4
 
 import utils.Resource
-import java.text.ParseException
 
 class Day4 {
 }
 
-data class Section(val start: Int, val end: Int)
+data class Section(val start: Int, val end: Int) {
+    companion object {
+        private fun parse(value: String): Section? {
+            val values = value.split("-")
+            if (values.count() != 2) {
+                return null
+            }
+
+            return try {
+                Section(
+                    start = values[0].toInt(),
+                    end = values[1].toInt()
+                )
+            } catch (e: NumberFormatException) {
+                null
+            }
+        }
+
+        fun parsePair(value: String): Pair<Section, Section>? {
+            val pairs = value.split(",")
+            if (pairs.count() != 2) {
+                return null
+            }
+
+            val first = Section.parse(pairs[0])
+            val second = Section.parse(pairs[1])
+            if (first == null || second == null) {
+                return null
+            }
+
+            return first to second
+        }
+    }
+}
 
 operator fun Section.contains(other: Section): Boolean = this.start <= other.start && this.end >= other.end
 fun Section.overlaps(other: Section): Boolean =
     other.start in this.start..this.end ||
             other.end in this.start..this.end
-
-fun parseSection(value: String): Section? {
-    val values = value.split("-")
-    if (values.count() != 2) {
-        return null
-    }
-
-    return try {
-        Section(
-            start = values[0].toInt(),
-            end = values[1].toInt()
-        )
-    } catch (e: NumberFormatException) {
-        null
-    }
-}
-
-fun parseSections(value: String): Pair<Section, Section>? {
-    val pairs = value.split(",")
-    if (pairs.count() != 2) {
-        return null
-    }
-
-    val first = parseSection(pairs[0])
-    val second = parseSection(pairs[1])
-    if (first == null || second == null) {
-        return null
-    }
-
-    return first to second
-}
 
 fun main() {
     val file = Resource.getFile("day4/input") ?: return
@@ -51,7 +52,7 @@ fun main() {
     var overlapsPairs = 0
 
     file.forEachLine {
-        val pair = parseSections(it) ?: throw Exception("input format is bad")
+        val pair = Section.parsePair(it) ?: throw Exception("input format is bad")
 
         if (pair.first in pair.second || pair.second in pair.first) {
             badPairs++
